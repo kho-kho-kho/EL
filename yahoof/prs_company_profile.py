a1c6d9a7eca9152750928364febcd90978c51ad7
k ho k ho? <6939499+kho-kho-kho@users.noreply.github.com>
2020-09-12 12:45:58 -0700

    Update prs_company_profile.py
    pass pylint

--- yahoof/prs_company_profile.py
+++ yahoof/prs_company_profile.py
@@ -15,37 +15,37 @@ INPUT = 'AAPL,'
 
 URL_BASE = 'https://finance.yahoo.com/quote/'
 
-random.seed()
-
-options = Options()
-options.add_argument("--incognito")
-options.headless = True
-driver = webdriver.Chrome(options=options)
-
-args = [(x, f'{URL_BASE}{x}') for x in INPUT.split(',') if x]
-
-for ticker, url in args:
-    driver.get(url)
-    src = driver.page_source
-
-    match = re.search(r'"summaryProfile"\s*:\s*{(.*?)}', src, re.DOTALL)
-    if match and match.group(1):
-        txt = match.group(1)
-        err = re.search(r'"err"\s*:\s*{', txt, re.S)
-        if err:
-            print(f'{ticker}|ERR')
-            continue
-        emp = re.search(r'"fullTimeEmployees"\s*:\s*(\d*)', txt, re.S).group(1)
-        ind = re.search(r'"industry"\s*:\s*"(.*?)"', txt, re.S).group(1)
-        sec = re.search(r'"sector"\s*:\s*"(.*?)"', txt, re.S).group(1)
-        print(f'{ticker}|{sec}|{ind}|{emp}')
-
-    time.sleep(random.randint(5, 9))
-
-driver.quit()
-
 def main():
     """ xxx """
+
+    random.seed()
+
+    options = Options()
+    options.add_argument("--incognito")
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+
+    args = [(x, f'{URL_BASE}{x}') for x in INPUT.split(',') if x]
+
+    for ticker, url in args:
+        driver.get(url)
+        src = driver.page_source
+
+        match = re.search(r'"summaryProfile"\s*:\s*{(.*?)}', src, re.DOTALL)
+        if match and match.group(1):
+            txt = match.group(1)
+            err = re.search(r'"err"\s*:\s*{', txt, re.S)
+            if err:
+                print(f'{ticker}|ERR')
+                continue
+            emp = re.search(r'"fullTimeEmployees"\s*:\s*(\d*)', txt, re.S).group(1)
+            ind = re.search(r'"industry"\s*:\s*"(.*?)"', txt, re.S).group(1)
+            sec = re.search(r'"sector"\s*:\s*"(.*?)"', txt, re.S).group(1)
+            print(f'{ticker}|{sec}|{ind}|{emp}')
+
+        time.sleep(random.randint(5, 9))
+
+    driver.quit()
 
 if __name__ == "__main__":
     main()
