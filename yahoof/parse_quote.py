9b7de4c4939ce0e979215a70c2b5aff7ac287726
k ho k ho? <6939499+kho-kho-kho@users.noreply.github.com>
2020-10-02 22:02:52 -0700

    refactor main into functions
    some data/unit cleaning
    output to console AND local file
    ignore csv files

--- yahoof/parse_quote.py
+++ yahoof/parse_quote.py
@@ -5,18 +5,79 @@ Created on Fri Sep  4 15:39:16 2020
 @author: KHO
 """
 
+import pandas as pd
 import random
 import re
 import time
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 INPUT = 'AAPL,'
+OUTPUT = 'output.csv'
 
 WAIT_MIN = 5
 WAIT_MAX = 9
 
 URL_BASE = 'https://finance.yahoo.com/quote/'
+YNA = 'N/A' # for matching missing values
+
+def _parse_stats(src, out):
+    """ xxx """
+
+    cap = bet = per = div = yld = -1.0
+    mc_units = {'B': 1, 'M': 0.001, 'T': 1000, }
+
+    regex = r'<table.*?>(.*?)</table>.*<table.*?>(.*?)</table>'
+    match = re.search(regex, src, re.S)
+    if match and match.group(1) and match.group(2):
+        tb2 = match.group(2)
+
+        reg = r'<span.*?>Market Cap</span>.*?<span.*?>(.*?)</span>'
+        mat = re.search(reg, tb2, re.S)
+        if mat and mat.group(1) and mat.group(1) != YNA:
+            cap = float(mat.group(1)[:-1]) * mc_units[mat.group(1)[-1:]]
+
+        reg = r'<span.*?>Beta.*?</span>.*?<span.*?>(.*?)</span>'
+        mat = re.search(reg, tb2, re.S)
+        if mat and mat.group(1) and mat.group(1) != YNA:
+            bet = mat.group(1)
+
+        reg = r'<span.*?>PE Ratio.*?</span>.*?<span.*?>(.*?)</span>'
+        mat = re.search(reg, tb2, re.S)
+        if mat and mat.group(1) and mat.group(1) != YNA:
+            per = mat.group(1)
+
+        reg = r'<span.*?>Forward Dividend.*?</span>.*?<td.*?>(.*?) \((.*?)%\)</td>'
+        mat = re.search(reg, tb2, re.S)
+        if mat and mat.group(1) and mat.group(1) != YNA and mat.group(2):
+            div = mat.group(1)
+            yld = mat.group(2)
+
+    out['M.Cap(1b)'] = cap
+    out['Beta(5ym)'] = bet
+    out['PER(TTM)'] = per
+    out['Fwd.Div$'] = div
+    out['Div.yld%'] = yld
+
+def _parse_profile(src, out):
+    """ xxx """
+
+    emp = -1.0
+    ind = sec = ''
+
+    match = re.search(r'"summaryProfile"\s*:\s*{(.*?)}', src, re.S)
+    if match and match.group(1):
+        pro = match.group(1)
+
+        if not re.search(r'"err"\s*:\s*{', pro, re.S):
+            emp = re.search(r'"fullTimeEmployees"\s*:\s*(\d*)', pro, re.S).group(1)
+            ind = re.search(r'"industry"\s*:\s*"(.*?)"', pro, re.S).group(1)
+            sec = re.search(r'"sector"\s*:\s*"(.*?)"', pro, re.S).group(1)
+
+    out['Emp(1k)'] = float(emp) / 1000
+    out['Sector'] = sec
+    out['Indus.'] = ind
 
 def main():
     """ xxx """
@@ -28,37 +89,20 @@ def main():
     options.headless = True
     driver = webdriver.Chrome(options=options)
 
-    args = [(x, f'{URL_BASE}{x}') for x in INPUT.split(',') if x]
-
-    for ticker, url in args:
+    rows = []
+    for name, url in [(x, f'{URL_BASE}{x}') for x in INPUT.split(',') if x]:
         driver.get(url)
-        src = driver.page_source
 
-        res = 'OK'
-        cap = bet = per = div = emp = ind = sec = ''
+        row = {'Name': name}
+        _parse_profile(driver.page_source, row)
+        _parse_stats(driver.page_source, row)
+        rows.append(row)
 
-        match = re.search(r'<table.*?>(.*?)</table>.*<table.*?>(.*?)</table>', src, re.S)
-        if match and match.group(1) and match.group(2):
-            tb2 = match.group(2)
-
-            cap = re.search(r'<span.*?>Market Cap</span>.*?<span.*?>(.*?)</span>', tb2, re.S).group(1)
-            bet = re.search(r'<span.*?>Beta.*?</span>.*?<span.*?>(.*?)</span>', tb2, re.S).group(1)
-            per = re.search(r'<span.*?>PE Ratio.*?</span>.*?<span.*?>(.*?)</span>', tb2, re.S).group(1)
-            div = re.search(r'<span.*?>Forward Dividend.*?</span>.*?<td.*?>(.*?)</td>', tb2, re.S).group(1)
-
-        match = re.search(r'"summaryProfile"\s*:\s*{(.*?)}', src, re.S)
-        if match and match.group(1):
-            pro = match.group(1)
-
-            if re.search(r'"err"\s*:\s*{', pro, re.S):
-                res = 'ERR'
-            else:
-                emp = re.search(r'"fullTimeEmployees"\s*:\s*(\d*)', pro, re.S).group(1)
-                ind = re.search(r'"industry"\s*:\s*"(.*?)"', pro, re.S).group(1)
-                sec = re.search(r'"sector"\s*:\s*"(.*?)"', pro, re.S).group(1)
-
-        print(f'{ticker}|{res}|{sec}|{ind}|{emp}|{cap}|{bet}|{per}|{div}')
+        print('|'.join([str(x) for x in row.values()]))
         time.sleep(random.randint(WAIT_MIN, WAIT_MAX))
+
+    fpath = '\\'.join([str(Path(__file__).parents[0]), OUTPUT])
+    pd.DataFrame(rows).to_csv(fpath)
 
     driver.quit()
 
